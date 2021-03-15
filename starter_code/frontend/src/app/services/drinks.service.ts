aba9d28ce774b5ec774fd4a48d863a1d6e5b4629
Matekuor <51318778+Tekuor@users.noreply.github.com>
2021-03-15 21:37:22 +0000

    removed console log

--- starter_code/frontend/src/app/services/drinks.service.ts
+++ starter_code/frontend/src/app/services/drinks.service.ts
@@ -95,7 +95,6 @@ export class DrinksService {
       this.http.get(this.url + '/drinks-detail', this.getHeaders())
       .subscribe((res: any) => {
         this.drinksToItems(res.drinks);
-        console.log(res);
       });
     } else {
       this.http.get(this.url + '/drinks', this.getHeaders())
